03840bcee5a277c77b577ec6a18590448f6dfa5b
Oscar Aguinagalde <oscaraguinagalde@gmail.com>
2022-09-26 10:40:59 +0900

    some cleaning

--- src/util.h
+++ src/util.h
@@ -6,3 +6,6 @@ int maximum(int a, int b);
 void swap(int& a, int& b);
 std::chrono::steady_clock::time_point measure_time();
 void measure_since(std::chrono::steady_clock::time_point start);
+
+#define MAX(a,b) ((a) > (b) ? (a) : (b))
+#define MIN(a,b) ((a) < (b) ? (a) : (b))
