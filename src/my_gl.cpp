b927c5a62c93066fd633750c16aa5fa3ed118a98
Oscar Aguinagalde <oscaraguinagalde@gmail.com>
2022-12-30 15:53:26 +0900

    Column major

--- src/my_gl.cpp
+++ src/my_gl.cpp
@@ -193,20 +193,20 @@ namespace gl {
 
         Matrix transformation_matrix = Matrix::identity();
         transformation_matrix[0][0] = x.x;
-        transformation_matrix[1][0] = x.y;
-        transformation_matrix[2][0] = x.z;
-
-        transformation_matrix[0][1] = y.x;
+        transformation_matrix[0][1] = x.y;
+        transformation_matrix[0][2] = x.z;
+        
+        transformation_matrix[1][0] = y.x;
         transformation_matrix[1][1] = y.y;
-        transformation_matrix[2][1] = y.z;
-
-        transformation_matrix[0][2] = z.x;
-        transformation_matrix[1][2] = z.y;
+        transformation_matrix[1][2] = y.z;
+        
+        transformation_matrix[2][0] = z.x;
+        transformation_matrix[2][1] = z.y;
         transformation_matrix[2][2] = z.z;
-
-        transformation_matrix[0][3] = (x * camera_location)  * -1 ;
-        transformation_matrix[1][3] = (y * camera_location)  * -1 ;
-        transformation_matrix[2][3] = (z * camera_location)  * -1 ;
+        
+        transformation_matrix[0][3] = (x * camera_location) * -1;
+        transformation_matrix[1][3] = (y * camera_location) * -1;
+        transformation_matrix[2][3] = (z * camera_location) * -1;
 
         return transformation_matrix;
     }
