5dd32ec0a28ac87d9af206a57d04af754c6e4dec
Oscar Aguinagalde <oscaraguinagalde@gmail.com>
2023-09-11 03:24:53 +0900

    wip: imgui and imgui platform/renderer implementation

--- build.zig
+++ build.zig
@@ -16,7 +16,22 @@ pub fn build(b: *Builder) !void {
 
     const win32 = b.createModule(.{
         // https://github.com/marlersoft/zigwin32 - e61d5e9 - 21.0.3-preview
-        .source_file = .{ .path = "../zigwin32/win32.zig" },
+        .source_file = .{ .path = "dep/zigwin32/win32.zig" },
     });
     exe.addModule("win32", win32);
+    
+    // imgui
+    exe.linkLibCpp();
+    exe.addCSourceFile(.{ .file = .{ .path = "dep/cimgui/imgui/imgui.cpp" }, .flags = &[_] []const u8 {""} });
+    exe.addCSourceFile(.{ .file = .{ .path = "dep/cimgui/imgui/imgui_draw.cpp" }, .flags = &[_] []const u8 {""} });
+    exe.addCSourceFile(.{ .file = .{ .path = "dep/cimgui/imgui/imgui_demo.cpp" }, .flags = &[_] []const u8 {""} });
+    exe.addCSourceFile(.{ .file = .{ .path = "dep/cimgui/imgui/imgui_tables.cpp" }, .flags = &[_] []const u8 {""} });
+    exe.addCSourceFile(.{ .file = .{ .path = "dep/cimgui/imgui/imgui_widgets.cpp" }, .flags = &[_] []const u8 {""} });
+    // exe.addCSourceFile(.{ .file = .{ .path = "dep/cimgui/imgui/backends/imgui_impl_win32.cpp" }, .flags = &[_] []const u8 {"-Idep/cimgui/imgui/", "-Idep/cimgui/imgui/backends/"} }); {
+    //     exe.linkSystemLibrary("dwmapi");
+    // }
+    // exe.addCSourceFile(.{ .file = .{ .path = "dep/cimgui/imgui/backends/imgui_impl_opengl3.cpp" }, .flags = &[_] []const u8 {"-Idep/cimgui/imgui/",} });
+    exe.addCSourceFile(.{ .file = .{ .path = "dep/cimgui/cimgui.cpp" }, .flags = &[_] []const u8 {""} });
+    exe.addIncludePath(.{ .path = "dep/cimgui" });
+    // exe.addIncludePath(.{ .path = "dep/cimgui/imgui/backends" });
 }
