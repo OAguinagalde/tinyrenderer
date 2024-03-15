d120ba4778baaef2a8b546b0a398982783481b2f
Oscar Aguinagalde <oscaraguinagalde@gmail.com>
2024-03-15 16:48:55 +0900

    Implement the Application::sound API for platform win32 based of waveout

--- src/windows.zig
+++ src/windows.zig
@@ -3,8 +3,12 @@ const builtin = @import("builtin");
 
 const math = @import("math.zig");
 const Vector2i = math.Vector2i;
+
 /// The actual `windows.h` API
-const win32 = @import("win32").everything;
+const win32 = @import("win32.zig");
+// const win32 = @import("win32").everything;
+// const win32 = @import("../dep/zigwin32/win32/everything.zig");
+
 const Buffer2D = @import("buffer.zig").Buffer2D;
 const BGRA = @import("pixels.zig").BGRA;
 
@@ -143,18 +147,11 @@ pub fn Application(comptime app: ApplicationDescription) type {
 
                 try app.init(app_long_allocator.allocator());
 
-                const devices = try sound.enumerate_sound_devices(app_long_allocator.allocator());
-                for (devices.strings.items) |device_name| {
-                    std.log.debug("sound device {s}", .{device_name});
-                }
-
-                try sound.waveout_setup(app_long_allocator.allocator(), .{});
-
                 var running: bool = true;
                 while (running) {
 
                     var ms: f32 = undefined;
-                    const do_artificial_wait = true;
+                    const do_artificial_wait = false;
                     while (true) {
                         var current_cpu_counter: win32.LARGE_INTEGER = undefined;
                         _ = win32.QueryPerformanceCounter(&current_cpu_counter);
@@ -376,6 +373,10 @@ pub fn Application(comptime app: ApplicationDescription) type {
             const bytes = try file.reader().readAllAlloc(allocator, file_stats.size);
             return bytes;
         }
+
+        pub const sound = struct {
+            pub const initialize = waveout.waveout_setup;
+        };
     };
 }
 
@@ -478,7 +479,7 @@ fn FixedBufferAllocatorWrapper(comptime name: []const u8, comptime log: bool) ty
 
 }
 
-const sound = struct {
+const waveout = struct {
 
     const Strings = struct {
         data: std.ArrayList(u8),
@@ -493,7 +494,7 @@ const sound = struct {
 
         // https://learn.microsoft.com/en-us/windows/win32/api/mmeapi/nf-mmeapi-waveoutgetnumdevs
         const device_count = win32.waveOutGetNumDevs();
-        if (device_count == 0) @panic("device count is 0?!"); // TODO error handling or at least report
+        if (device_count == 0) return error.NoSoundDevicesOrErrorHappened;
 
         // https://learn.microsoft.com/en-us/windows/win32/api/mmeapi/nf-mmeapi-waveoutgetdevcaps
         var string_data = try std.ArrayList(u8).initCapacity(allocator, 32*16);
@@ -501,19 +502,20 @@ const sound = struct {
         var i: u32 = 0;
         while (i < device_count) : (i += 1) {
             var capabilities: win32.WAVEOUTCAPSA = undefined;
-            const result = win32.waveOutGetDevCapsA(i, &capabilities, @sizeOf(win32.WAVEOUTCAPSA));
-            if (result == win32.S_OK) {
-                const lenght = blk: {
-                    for (&capabilities.szPname, 0..) |b, l| if (b == 0) break :blk l;
-                    break :blk 32;
-                };
-                const str = try string_data.addManyAsSlice(lenght);
-                @memcpy(str, capabilities.szPname[0..lenght]);
-                try strings.append(str);
+            switch(win32.waveOutGetDevCapsA(i, &capabilities, @sizeOf(win32.WAVEOUTCAPSA))) {
+                win32.MMSYSERR_NOERROR => {},
+                win32.MMSYSERR_BADDEVICEID => {std.log.err("waveOutGetDevCapsA on sound device {}: Specified device identifier is out of range.", .{i});},
+                win32.MMSYSERR_NODRIVER => {std.log.err("waveOutGetDevCapsA on sound device {}: No device driver is present.", .{i});},
+                win32.MMSYSERR_NOMEM => {std.log.err("waveOutGetDevCapsA on sound device {}: Unable to allocate or lock memory.", .{i});},
+                else => unreachable
             }
-            else {
-                // TODO ith device couldn't be read, report it
-            }
+            const lenght = blk: {
+                for (&capabilities.szPname, 0..) |b, l| if (b == 0) break :blk l;
+                break :blk 32;
+            };
+            const str = try string_data.addManyAsSlice(lenght);
+            @memcpy(str, capabilities.szPname[0..lenght]);
+            try strings.append(str);
         }
 
         return .{
@@ -523,7 +525,7 @@ const sound = struct {
         
     }
 
-    const SampleType = u16;
+    const SampleType = i16;
 
     const Context = struct {
         waveout_handle: win32.HWAVEOUT,
@@ -543,18 +545,55 @@ const sound = struct {
     
     var context: Context = undefined;
 
-    fn default(time: f64) SampleType {
-        std.log.debug("time {}", .{time});
-        return @intFromFloat(@as(f64, std.math.sin(time)*@as(f64, std.math.maxInt(SampleType))));
+    var number: SampleType = 0;
+    
+    /// returns -1 to +1
+    fn default(time: f64) f64 {
+        switch (3) {
+            0 => {
+                const max: f64 = std.math.maxInt(SampleType)/2;
+                const sin: f64 = std.math.sin(time)+1;
+                const val: SampleType = @intFromFloat(sin*max);
+                return val;
+            },
+            1 => {
+                return 0;
+            },
+            2 => {
+                number, _ = @addWithOverflow(number, 1);
+                return number;
+            },
+            3 => {
+                
+                // |   |   |   |   |   | |   |   |   |   | |   | |   |   |   |
+                // |   | S |   |   | F | | G |   |   | J | | K | | L |   |   |
+                // |   |___|   |   |___| |___|   |   |___| |___| |___|   |   |__
+                // |     |     |     |     |     |     |     |     |     |     |
+                // |  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  /  |
+                // |_____|_____|_____|_____|_____|_____|_____|_____|_____|_____|
+                //    Z  S  X     C  F  V  G  B     N  J  M  K  ,  L  .     /
+                //    0  1  2     3  4  5  6  7     8  9  10 11 12 13 14    15
+                const key = 15;
+                const octave_base_frequency: f64 = 110; // A2
+                const _12th_root_of_2: f64 = std.math.pow(f64, 2, 1/12);
+                const frequency_output: f64 = std.math.pow(f64, _12th_root_of_2, key) * octave_base_frequency;
+                return std.math.sin(time * 2 * 3.14159 * frequency_output * 0.5);
+            },
+            4 => {
+                return 0;
+            },
+            else => unreachable
+        }
     }
     
-    const Config = struct {
+    pub const Config = struct {
+        // TODO WAVE_MAPPER points to default syste, sound device or something like that, look into that
         device_index: u32 = 0,
         samples_per_second: usize = 44100,
         channels: usize = 1,
         block_count: usize = 8,
         block_sample_count: usize = 256,
-        user_callback: *const fn (time: f64) SampleType = &default,
+        user_callback: *const fn (time: f64) f64 = &default,
     };
 
     fn waveOutProc(hwo: win32.HWAVEOUT, uMsg: u32, dwInstance: *u32, dwParam1: *u32, dwParam2: *u32) void {
@@ -587,8 +626,16 @@ const sound = struct {
         waveout_desired_format.cbSize = 0;
         
         var waveout_handle: ?win32.HWAVEOUT = undefined;
-        const result = win32.waveOutOpen(&waveout_handle, config.device_index, &waveout_desired_format, @intFromPtr(&waveOutProc), 0, win32.CALLBACK_FUNCTION);
-        if (result != win32.S_OK) @panic("failed to create waveout context!");
+        switch (win32.waveOutOpen(&waveout_handle, config.device_index, &waveout_desired_format, @intFromPtr(&waveOutProc), 0, win32.CALLBACK_FUNCTION)) {
+            win32.MMSYSERR_NOERROR => {},
+            win32.MMSYSERR_ALLOCATED => {std.log.err("Specified resource is already allocated.", .{});},
+            win32.MMSYSERR_BADDEVICEID => {std.log.err("Specified device identifier is out of range.", .{});},
+            win32.MMSYSERR_NODRIVER => {std.log.err("No device driver is present.", .{});},
+            win32.MMSYSERR_NOMEM => {std.log.err("Unable to allocate or lock memory.", .{});},
+            win32.WAVERR_BADFORMAT => {std.log.err("Attempted to open with an unsupported waveform-audio format.", .{});},
+            win32.WAVERR_SYNC => {std.log.err("The device is synchronous but waveOutOpen was called without using the WAVE_ALLOWSYNC flag.", .{});},
+            else => unreachable
+        }
 
         const waveout_sample_buffer = try allocator.alloc(SampleType, config.block_count * config.block_sample_count);
         @memset(waveout_sample_buffer, 0);
@@ -632,19 +679,43 @@ const sound = struct {
             context.sample_block_free_count_mutex.unlock();
 
             if (context.sample_block_descriptors[context.sample_block_current_index].dwFlags & win32.WHDR_PREPARED != 0) {
-                _ = win32.waveOutUnprepareHeader(context.waveout_handle, &context.sample_block_descriptors[context.sample_block_current_index], @sizeOf(win32.WAVEHDR));
+                switch (win32.waveOutUnprepareHeader(context.waveout_handle, &context.sample_block_descriptors[context.sample_block_current_index], @sizeOf(win32.WAVEHDR))) {
+                    win32.MMSYSERR_NOERROR => {},
+                    win32.MMSYSERR_INVALHANDLE => {std.log.err("Specified device handle is invalid.", .{});},
+                    win32.MMSYSERR_NODRIVER => {std.log.err("No device driver is present.", .{});},
+                    win32.MMSYSERR_NOMEM => {std.log.err("Unable to allocate or lock memory.", .{});},
+                    win32.WAVERR_STILLPLAYING => {std.log.err("The data block pointed to by the pwh parameter is still in the queue.", .{});},
+                    else => unreachable
+                }
             }
 
             // TODO factor in that there might be more than 1 channel
             const index_of_first_sample = context.sample_block_current_index * context.waveout_config.block_sample_count;
             const sample_block: []SampleType = context.sample_buffer[index_of_first_sample .. index_of_first_sample + context.waveout_config.block_sample_count];
             for (sample_block) |*sample| {
-                sample.* = context.waveout_config.user_callback(time);
+                const max_sample_as_f64 = @as(f64, @floatFromInt(std.math.maxInt(SampleType)));
+                const new_sample = std.math.clamp(context.waveout_config.user_callback(time), -1, 1);
+                sample.* = @intFromFloat(new_sample * max_sample_as_f64);
                 time += seconds_per_sample;
             }
 
-            _ = win32.waveOutPrepareHeader(context.waveout_handle, &context.sample_block_descriptors[context.sample_block_current_index], @sizeOf(win32.WAVEHDR));
-            _ = win32.waveOutWrite(context.waveout_handle, &context.sample_block_descriptors[context.sample_block_current_index], @sizeOf(win32.WAVEHDR));
+            switch (win32.waveOutPrepareHeader(context.waveout_handle, &context.sample_block_descriptors[context.sample_block_current_index], @sizeOf(win32.WAVEHDR))) {
+                win32.MMSYSERR_NOERROR => {},
+                win32.MMSYSERR_INVALHANDLE => {std.log.err("Specified device handle is invalid.", .{});},
+                win32.MMSYSERR_NODRIVER => {std.log.err("No device driver is present.", .{});},
+                win32.MMSYSERR_NOMEM => {std.log.err("Unable to allocate or lock memory.", .{});},
+                else => unreachable
+            }
+
+            switch (win32.waveOutWrite(context.waveout_handle, &context.sample_block_descriptors[context.sample_block_current_index], @sizeOf(win32.WAVEHDR))) {
+                win32.MMSYSERR_NOERROR => {},
+                win32.MMSYSERR_INVALHANDLE => {std.log.err("Specified device handle is invalid.", .{});},
+                win32.MMSYSERR_NODRIVER => {std.log.err("No device driver is present.", .{});},
+                win32.MMSYSERR_NOMEM => {std.log.err("Unable to allocate or lock memory.", .{});},
+                win32.WAVERR_UNPREPARED => {std.log.err("The data block pointed to by the pwh parameter hasn't been prepared.", .{});},
+                else => unreachable
+            }
+
             context.sample_block_current_index = @mod(context.sample_block_current_index+1, context.waveout_config.block_count);
         }
     }
